0091aa83af9f64ece74f6b7190a6a3c7f52394c6
Mark Heatzig <markheatzig@gmail.com>
2023-06-29 22:15:47 -0400

    v2 of fanatics scraper

--- fanatics.py
+++ fanatics.py
@@ -22,7 +22,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 # Define the columns for your DataFrame
-columns = ['Sport', 'Team', 'Product ID', 'Product Name', 'Product Price']
+columns = ['Sport', 'Team', 'Product ID', 'Product Name', 'Product Price', 'Brand Name', 'Product URL']
 rows = []
 
 driver = webdriver.Chrome()
@@ -37,91 +37,90 @@ def back_home(driver, sport):
 
 
 def check_products(driver):
-    driver.get('https://www.fanatics.com')
+    
     
     # Create the DataFrame
     df = pd.DataFrame(columns=columns)
-    print(type(df))
-    dataframe = df
+
+
     # break_dict = {"NFL":32, "MLB": 50, "NBA": 50, "MLS": 50, "Soccer": 48}
     # sports = ["NFL", "MLB", "NBA", "MLS", "Soccer"]
+
+
     # break_dict = {"MLB": 50, "NBA": 50, "MLS": 50, "Soccer": 48}
     # sports = ["MLB", "NBA", "MLS", "Soccer"]
 
-    # break_dict = {"NBA": 50, "MLS": 50, "Soccer": 48}
-    # sports = ["NBA", "MLS", "Soccer"]
-    break_dict = {"NHL": 48}
-    sports = ["NHL"]
+    # break_dict = {"NHL": 50, "Soccer": 48}
+    # sports = ["NHL", "Soccer"]
+
+    break_dict = {"NCAA": 50}
+    sports = ["NCAA"]
 
     for sport in sports:
+        print("We are on sport: ", sport)
+        driver.get('https://www.fanatics.com')
         rows = []
         sport_logo = WebDriverWait(driver, 15).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, f'a[data-trk-id="primary-selector-{sport}"]'))
         )
         sport_logo.click()
-        sleep(2)
+        sleep(1)
         source = driver.page_source
         soup = BeautifulSoup(source, 'html.parser')
         secondary_selector_items = soup.find_all(class_='secondary-selector-item')
         i=0
         for item in secondary_selector_items:
+            print('We are on item: ', i, ' of ', len(secondary_selector_items))
             i+=1
             if i == break_dict[sport]:
                 break
-            print('We are on item: ', i, ' of ', len(secondary_selector_items))
             data_trk_id = item.a['data-trk-id']
             print(data_trk_id)
-            sleep(1)
+            sleep(3)
+            try:
+                team_link = WebDriverWait(driver, 15).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, f'a[data-trk-id="{data_trk_id}"]'))
+                )
+                team_link.click()
+                sleep(3)
+            except selenium.common.exceptions.TimeoutException:
+                print(f"No 'team' link found for {data_trk_id}. Continuing to next item.")
+                back_home(driver, sport)
 
-            team_link = WebDriverWait(driver, 15).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, f'a[data-trk-id="{data_trk_id}"]'))
-            )
-            team_link.click()
-            sleep(1)
+                continue
+
             try:
                 baby_link = WebDriverWait(driver, 15).until(
                     EC.presence_of_element_located((By.CSS_SELECTOR, 'a[data-trk-id="baby"]'))
                 )
                 baby_link.click()
-                sleep(1)
+                sleep(3)
             except selenium.common.exceptions.TimeoutException:
                 print(f"No 'baby' link found for {data_trk_id}. Continuing to next item.")
+                back_home(driver, sport)
+
                 continue
 
-            soup = BeautifulSoup(driver.page_source, 'html.parser')
-            product = soup.find('div', class_='grid-small-1-medium-3 row small-up-1 medium-up-3')
 
-            product_containers = soup.find_all('div', class_='product-card row')
-            for product in product_containers:
-                try:
-                    # find the product title element
-                    product_title_element = product.find('div', class_='product-card-title').find('a')
-                    
-                    # extract product ID
-                    product_id = product_title_element.get('data-trk-id')
-                    print(f'Product ID: {product_id}')
-                    
-                    # extract product name
-                    product_name = product_title_element.text.strip()
-                    print(f'Product Name: {product_name}')
-                    
-                    # extract product price
-                    product_price_element = product.find('div', class_='price-card').find('span', class_='sr-only')
-                    if product_price_element is not None:
-                        product_price = product_price_element.text.strip()
-                        print(f'Product Price: {product_price}\n')
-                    else:
-                        print('Product Price: Not found\n')
-                    new_row = [sport, data_trk_id, product_id, product_name, product_price]
-                    rows.append(new_row)
-                    df = pd.DataFrame(rows, columns=columns)
-                    df.to_csv(f"{sport}_output.csv", index=False)
-                except selenium.common.exceptions.TimeoutException:
-                    print(f"TimeoutException for {data_trk_id}. Continuing to next item.")
-                    continue
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+            script_tag = soup.find('script', {'type': 'application/ld+json'})
+            product_listing_data = json.loads(script_tag.string)
+
+            for product in product_listing_data:
+                # Extract the required information
+                product_name = product.get('name')
+                product_id = product['offers'][0].get('serialNumber')
+                product_price = product['offers'][0]['priceSpecification'].get('price')
+                url = product.get('url')
+                brand_name = product.get('brand')
+                new_row = [sport, data_trk_id, product_id, product_name, product_price, brand_name, url]
+                rows.append(new_row)
+            df = pd.DataFrame(rows, columns=columns)
+            df.to_csv(f"{sport}_output.csv", index=False)
             back_home(driver, sport)
-        print('We are saving the following DataFrame to a CSV file: \n')
-        back_home(driver, sport)
+            
+        print('Finished with sport: ', sport)
 
     return rows
 
